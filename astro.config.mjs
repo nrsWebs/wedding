5203c6330d3264b2bcdbd2716a64c1486dc7bd0b
cgarcia <cgarcia@omitsis.com>
2025-03-10 19:40:00 +0100

    Deploy changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://nrsWebs.github.io',
-
+  base: 'wedding',
   vite: {
     plugins: [tailwindcss()]
   }
