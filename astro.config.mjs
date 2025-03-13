6eaae50a3a808e3ed5ea5eb1419bdd4d7ff0bc22
cgarcia <cgarcia@omitsis.com>
2025-03-13 18:56:44 +0100

    Set domain for astro deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://nrsWebs.github.io',
+  site: 'http://matchpatolavida.com/',
   base: 'wedding',
   vite: {
     plugins: [tailwindcss()]
