31d3f55762a3cd9d8c83d2329ed0fd16d1b0e93e
cgarcia <cgarcia@omitsis.com>
2025-05-13 08:54:30 +0200

    version 1.0 de la fase 2

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
+import alpine from '@astrojs/alpinejs';
 
 // https://astro.build/config
 export default defineConfig({
@@ -9,5 +9,12 @@ export default defineConfig({
   // base: 'wedding',
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+  experimental: {
+    svg: {
+      mode: 'sprite',
+    },
+  },
+
+  integrations: [alpine({ entrypoint: '/src/entrypoint' })],  
 });
